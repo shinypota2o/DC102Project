1273f1b10d29873a70676c2b6894a66bfe4c8841
shinypota2o <katerapio@gmail.com>
2026-05-25 19:57:21 +0800

    update

--- admin/save_category.php
+++ admin/save_category.php
@@ -1,10 +1,10 @@
 <?php
 $con = mysqli_connect("localhost", "root", "", "bentaph");
 
-$category_name = $_POST["category_name"];
+$categoryname = $_POST["categoryname"];
 $description = $_POST["description"];
 
-mysqli_query($con, "insert into categories(category_name, description) values('$category_name','$description')");
+mysqli_query($con, "insert into categories(categoryname, description) values('$categoryname','$description')");
 
 echo "<script>window.location = 'categories.php'; </script>";
 ?>
